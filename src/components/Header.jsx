a1089b1a568556daab983b35f76462310b6266aa
Aldo Gonzalez <aldogonjuarez@.com>
2025-03-10 23:56:40 -0600

    creation of the navbar and header components

---
+++ src/components/Header.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import logoUrl from '../assets/images/logoGridesh.png'
+import logoUpavUrl from '../assets/images/upav-logo.png'
+
+const Header = () => {
+  return (
+    <header className="flex flex-col ">
+      <section className="flex flex-col flex-nowrap justify-center space-x-2 py-2 bg-orange-700 h-6">
+
+      </section>
+      <section className='flex flex-row justify-center items-center py-2 space-x-20 h-36'>
+        <div className='flex justify-center items-center'>
+          <img 
+            className="hidden md:block max-w-[200px] max-h-[100px] w-auto h-auto object-contain"
+            src={logoUrl} 
+            alt="logoGridesh" 
+          />
+        </div>
+        <div className='flex justify-center items-center'>
+          <h1 className='text-3xl font-sans'>COMPLEJO UNIVERSITARIO GRIDEHS</h1>
+        </div>
+        <div>
+          <img 
+            className="hidden md:block max-w-[250px] max-h-[150px] w-auto h-auto object-contain"
+            src={logoUpavUrl} 
+            alt="logo UPAV" 
+          />
+        </div>
+      </section> 
+    </header>
+  )
+}
+
+export default Header
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,19 @@
-export const Navbar = () => {
-    
+import React from "react";
+
+const Navbar = () => {
+    return (
+        <nav className="flex flex-col flex-nowrap justify-center space-x-2 py-2 bg-blue-950 h-12">
+          <section className="flex flex-row justify-center space-x-14">
+            <button className="btn-nav">Sobre Nosotros</button>
+            <button className="btn-nav">Bachillerato</button>
+            <button className="btn-nav">Licenciaturas</button>
+            <button className="btn-nav">Posgrados</button>
+            <button className="btn-nav">Cursos</button>
+            <button className="btn-nav">Eventos</button>
+            <button className="btn-nav">Actividades</button>
+          </section>
+        </nav>
+    )
 }
+
+export default Navbar;
